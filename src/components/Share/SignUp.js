877efe21ebfdaaa69a24a4f3e8cc633c15f65f2d
MdIbrahimKholil7 <kholilmdibrahim17@gmail.com>
2022-08-20 00:58:14 +0600

    form add

--- src/components/Share/SignUp.js
+++ src/components/Share/SignUp.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import { FaBeer } from 'react-icons/fa';
 import logo from '../../assets/logo.png'
+import axios from 'axios'
 const SignUp = () => {
+
+    const handleSubmit=async e=>{
+        e.preventDefault()
+        const {name,email,password}=e.target
+        
+        const {data}=await axios.post(`http://localhost:5000/chat/api/signup`,{name:name.value,email:email.value})
+        console.log(data)
+    }
     return (
         <div>
             <div className='w-full h-screen'>
@@ -16,30 +25,32 @@ const SignUp = () => {
                 <div className='flex justify-center items-center h-full mt-[-160px]'>
                     <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                         <div class="card-body">
-                            <div class="form-control">
-                                <label class="label">
-                                    <span class="label-text">Name</span>
-                                </label>
-                                <input type="text" placeholder="Name" class="input input-bordered" />
-                            </div>
-                            <div class="form-control">
-                                <label class="label">
-                                    <span class="label-text">Email</span>
-                                </label>
-                                <input type="text" placeholder="Email" class="input input-bordered" />
-                            </div>
-                            <div class="form-control">
-                                <label class="label">
-                                    <span class="label-text">Password</span>
-                                </label>
-                                <input type="text" placeholder="Password" class="input input-bordered" />
-                                <label class="label">
-                                    <a>Forgot password?</a>
-                                </label>
-                            </div>
-                            <div class="form-control mt-6">
-                                <button class="btn btn-primary">Login</button>
-                            </div>
+                            <form onSubmit={handleSubmit}>
+                                <div class="form-control">
+                                    <label class="label">
+                                        <span class="label-text">Name</span>
+                                    </label>
+                                    <input type="text" placeholder="Name" name='name' class="input input-bordered" required />
+                                </div>
+                                <div class="form-control">
+                                    <label class="label">
+                                        <span class="label-text">Email</span>
+                                    </label>
+                                    <input type="text" placeholder="Email" name='email' class="input input-bordered" required />
+                                </div>
+                                <div class="form-control">
+                                    <label class="label">
+                                        <span class="label-text">Password</span>
+                                    </label>
+                                    <input type="text" placeholder="Password" name='password' class="input input-bordered" required />
+                                    <label class="label">
+                                        <a>Forgot password?</a>
+                                    </label>
+                                </div>
+                                <div class="form-control mt-6">
+                                    <button type='submit' class="btn btn-primary">Login</button>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
